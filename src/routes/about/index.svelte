1a54527a3f21f1d94b4b17cb964bcf19a2b2c7e4
Lucretius Biah <lucretiusbiah@protonmail.com>
2021-09-14 07:59:29 -0700

    added certificates

---
+++ src/routes/about/index.svelte
@@ -0,0 +1,114 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script type="text/javascript">
+	import SEO from 'svelte-seo';
+</script>
+
+<SEO
+	title="Kudadam • About"
+	description="A page about me. Info contained here is my name and other important stuff about me"
+	keywords="about kudadam,about kudadam website,about lucretius, about lucretius biah"
+	canonical="https://www.kudadam.com/about"
+	openGraph={{
+		title: 'Kudadam • About',
+		description:
+			'A page about me. Info contained here is my name and other important stuff about me',
+		url: 'https://www.kudadam.com/about',
+		type: 'profile',
+		images: [
+			{
+				url: 'https:kudadam.sirv.com/logo/logo.png',
+				width: 850,
+				height: 650,
+				alt: 'Kudadam Logo'
+			}
+		]
+	}}
+	twitter={{
+		site: '@lucretius_1',
+		title: 'Kudadam • About',
+		description:
+			'A page about me. Info contained here is my name and other important stuff about me',
+		image: 'https://kudadam.sirv.com/logo/logo.png',
+		imageAlt: 'Logo of Kudadam'
+	}}
+/>
+
+<h1 class="text-center font-bold text-red-500">About me</h1>
+<div class="md:px-24">
+	<img
+		src="https://kudadam.sirv.com/profile/lucretius.jpg?thumbnail=300&q=90"
+		alt=""
+		class="my-4 p-1 border rounded mx-auto"
+	/>
+	<p>
+		Hello World <img
+			alt="👋"
+			class="emojione"
+			src="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/png/1f44b.png"
+			title=":wave:"
+		/>, Am Lucretius Biah
+		<img
+			alt="👦"
+			class="emojione"
+			src="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/png/1f466.png"
+			title=":boy:"
+		/><br /> Well, to describe myself on this page is a bit difficult as it is hard to find the
+		right words to use.<br /> Anyway, let's see how this ends...
+	</p>
+	<p>
+		My name is Lucretius and welcome to my part of the internet.<br /> Am a 17 y/o self-taught developer
+		and I can code in 8+ programming languages but my favourites are Python, JavaScript, Ruby and C++.
+	</p>
+	<p>
+		Am based somewhere in Wakanda🇬🇭.<br /> Professionally, am a full stack developer. For Front-end,
+		I normally use HTML, JavaScript and CSS or Sass. Then for backend development, I use PHP or Python
+		or Ruby. When it comes to handling databases, I use MySQL. Also, I use Java when am developing android
+		applications.
+	</p>
+	<p>
+		<strong>When am not coding...</strong><br /> Am a bookworm<img
+			alt="📘"
+			class="emojione"
+			src="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/png/1f4d8.png"
+			title=":blue_book:"
+		/>. My favourite story book is
+		<a href="https://en.wikipedia.org/wiki/The_Hardy_Boys">Hardy Boys</a>
+		by <a href="https://en.wikipedia.org/wiki/Franklin_W._Dixon">Franklin Dixon</a>.<br /> It's a
+		series, am yet to finish it.<br /> Am also a gamer, I mostly complete every game I start
+		<em class="text-muted">(Not bragging)</em>. I have completed about 35+ games without cheats<img
+			alt="😎"
+			class="emojione"
+			src="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/png/1f60e.png"
+			title=":sunglasses:"
+		/>.<br /> I love music too, but I don't have a favourite.<br /> I write in my diary in my spare time,
+		code when I have a computer close-by and play games when am bored.
+	</p>
+	<p>That's all I can say for now.<br /> Below are some stats about me</p>
+</div>
+<div class="flex flex-wrap gap-3 my-2">
+	<div class="">
+		<img
+			src="https://github-readme-stats.vercel.app/api?username=biah&count_private=true&show_icons=true&theme=radical"
+			alt=""
+		/>
+	</div>
+	<div class="">
+		<img
+			src="https://github-readme-stats.vercel.app/api/top-langs/?username=biah&langs_count=9"
+			alt=""
+		/>
+	</div>
+	<div class="">
+		<img src="https://github-readme-stats.vercel.app/api/wakatime?username=Lucretius" alt="" />
+	</div>
+</div>
+
+<style type="text/css">
+	.emojione {
+		height: 1.2rem;
+		display: inline;
+	}
+</style>
