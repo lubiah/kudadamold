58fb6b3b9c13220e17bb41573fd3b1bbcd2222bd
Lucretius Biah <lucretiusbiah@protonmail.com>
2021-09-18 05:34:01 -0700

    Fixed html minifier bug

--- src/hooks.js
+++ src/hooks.js
@@ -1,32 +1,30 @@
-// import { minify } from "html-minifier";	
+import { minify } from 'html-minifier';
 
-// const minification_options = {
-// 	collapseBooleanAttributes: true,
-// 	collapseWhitespace: true,
-// 	conservativeCollapse: true,
-// 	decodeEntities: true,
-// 	html5: true,
-// 	ignoreCustomComments: [/^#/],
-// 	minifyCSS: true,
-// 	minifyJS: false,
-// 	removeAttributeQuotes: true,
-// 	removeComments: true,
-// 	removeOptionalTags: true,
-// 	removeRedundantAttributes: true,
-// 	removeScriptTypeAttributes: true,
-// 	removeStyleLinkTypeAttributes: true,
-// 	sortAttributes: true,
-// 	sortClassName: true
-// };
+const minification_options = {
+	collapseBooleanAttributes: true,
+	collapseWhitespace: true,
+	conservativeCollapse: true,
+	decodeEntities: true,
+	html5: true,
+	ignoreCustomComments: [/^#/],
+	minifyCSS: true,
+	minifyJS: false,
+	removeAttributeQuotes: true,
+	removeComments: true,
+	removeOptionalTags: true,
+	removeRedundantAttributes: true,
+	removeScriptTypeAttributes: true,
+	removeStyleLinkTypeAttributes: true,
+	sortAttributes: true,
+	sortClassName: true
+};
 
+export async function handle({ request, resolve }) {
+  const response = await resolve(request);
 
-// export const handle = async ({ request, resolve })=>{
-// 	const response = await resolve(request);
+  if (response.headers['content-type'] === 'text/html') {
+    response.body = minify(response.body, minification_options);
+  }
 
-// 	if (response.headers["content-type"] === "text/html") {
-// 		response.body = minify(response.body, minification_options);
-
-// 	}
-
-// 	return response;
-// }
+  return response;
+}
