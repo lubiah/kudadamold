046a6cdecbda7c2313166f39363ee727d1097fd2
Lucretius Biah <lucretiusbiah@protonmail.com>
2021-10-15 17:52:16 +0000

    Finally fixed the flicker of dark mode

--- src/lib/Components/Navbar.svelte
+++ src/lib/Components/Navbar.svelte
@@ -47,16 +47,19 @@
 </script>
 
 <svelte:head>
-	{#if browser}
-		{#if $mode === 'dark' || (window.matchMedia('(prefers-color-scheme: dark)').matches) }
-			{document.documentElement.classList.add('dark')}
-			{mode.set("dark")}
-		{:else}
-			{document.documentElement.classList.remove('dark')}
-			{mode.set("light")}
-
-		{/if}
-	{/if}
+	<script>
+		if (document){
+			let mode = localStorage.theme || "light";
+			if (mode === 'dark' || (window.matchMedia('(prefers-color-scheme: dark)').matches)){
+				document.documentElement.classList.add("dark");
+				localStorage.theme = "dark";
+			}
+			else {
+				document.documentElement.classList.remove("dark");
+				localStorage.theme = "light";
+			}
+		}
+	</script>
 </svelte:head>
 
 <nav id="header" 
