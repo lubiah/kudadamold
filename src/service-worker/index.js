069710df00c9ae2e7e2007fc704bca08c045d934
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-02-04 02:03:27 +0000

    about to pull

--- src/service-worker/index.js
+++ src/service-worker/index.js
@@ -39,26 +39,6 @@ self.addEventListener("activate", event =>{
     console.info("[service worker] activated")
 })
 
-
-self.addEventListener('fetch', event => {
-    if (event.request.method !== 'GET' || event.request.headers.has('range')) return;
-
-    event.respondWith(
-        caches.match(event.request)
-        .then(response=>{
-            if (response)
-                return response;
-            return fetch(event.request)
-            .then(response=>{
-                if (!response || response.status !== 200)
-                    return response;
-                return caches.open(dynamic_cache)
-                .then(cache=>{
-                    cache.put(event.request.url,response.clone());
-                    return response;
-                })
-            })
-        })
-        )
-
-});
+self.addEventListener("fetch", event => {
+    
+})
