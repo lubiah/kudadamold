b39d8d79efd331d4de1889a848a6b78fe239b01f
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-03-20 03:57:09 +0000

    fixed a bug which was not allowing sitemap to be generated

--- scripts/build.bash
+++ scripts/build.bash
@@ -6,5 +6,5 @@ npm install
 touch tmp/restart.txt
 sleep 5 # Makes sure the server has restarted before it goes on to generate the sitemap
 rm sitemap.xml #removes an existing sitemap file if it exists
-npm install sitemap-generator && node generateSitemap.js && npm uninstall sitemap-generator
+npm install sitemap-generator && node scripts/generateSitemap.js && npm uninstall sitemap-generator
 rm -rf static src .svelte-kit .gitignore .prettierrc .jsconfig.json mdsvex.config.js README.md tailwind.config.cjs postcss.config.cjs .eslintrc.cjs jsconfig.json
