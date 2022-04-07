11e74cee2ef4fdccb1447a8dd1052d16eee04290
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-04-07 08:49:02 +0000

    Improved the matcher

--- src/params/post.js
+++ src/params/post.js
@@ -1,3 +1,8 @@
 export const match = params =>{
-    return params !== "feed"
+    let data = import.meta.glob("/src/routes/blog/_blog/**/*.md");
+    let array = new Array();
+    for (const datum in data){
+        array.push(/\/src\/routes\/blog\/_blog\/([a-z0-9]+(?:-[a-z0-9]+)*)\/index.md/gm.exec(datum)[1])
+    }
+    return array.find(e=>e === params)
 }
