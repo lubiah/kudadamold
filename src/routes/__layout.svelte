5bbdf7005e6c49572ec257d113ff1c2949da245a
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-06-11 00:32:54 +0000

    Changed the tracking code

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -18,12 +18,12 @@
 <head>
 	<link rel="alternate" type="application/rss+xml" href="https://www.kudadam.com/blog/feed.xml">
 	<link rel="preconnect" href="https://ik.imagekit.io/kudadam">
-	<script async src="//static.getclicky.com/101367229.js"></script>
+	<script async src="//static.getclicky.com/101369509.js"></script>
 </head>
 
 <div class="container mx-auto min-h-screen px-3 mt-20" id="layout_root">
 	<slot></slot>
 </div>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101367229ns.gif"></p></noscript>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101369509ns.gif" /></p></noscript>
 <Footer/>
 <Update/>
