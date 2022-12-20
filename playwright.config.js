a3400b2d0745f38fa457b9e0479e5f2e5c1963c1
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-12-20 23:56:16 +0000

    Made some changes to playwright

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,8 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+	maxFailures: 5,
+	fullyParallel: true,
+	workers: "100%",
 	webServer: {
 		command: "npm run build && npm run preview",
 		port: 4173
