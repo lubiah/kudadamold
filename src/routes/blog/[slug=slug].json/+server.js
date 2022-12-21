19927810f02827ce9a633a383c6d18eea7b8a0fd
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-12-21 18:00:55 +0000

    The blog data file is not prerendered

--- src/routes/blog/[slug=slug].json/+server.js
+++ src/routes/blog/[slug=slug].json/+server.js
@@ -1,5 +1,7 @@
 import { json } from "@sveltejs/kit";
 
+export const prerender = false;
+
 /**@type {import("./$types").RequestHandler} */
 export const GET = async ({ getClientAddress, params }) => {
 	//TODO: Only allow requests from my website
