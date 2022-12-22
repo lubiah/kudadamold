b7224abcc1774051ae2fa9c200d4c4ffadab3e4f
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-12-22 17:26:30 +0000

    Kept initDB function inside trycatch block

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,13 +1,18 @@
 import mysql from "mysql2/promise";
 import { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DATABASE } from "$env/static/private";
+import { dev } from "$app/environment";
 
 export const initDB = async () => {
-	const DB = mysql.createPool({
-		host: "localhost",
-		user: MYSQL_USERNAME,
-		password: MYSQL_PASSWORD,
-		database: MYSQL_DATABASE
-	});
-	await DB.getConnection();
-	return DB;
+	try {
+		const DB = mysql.createPool({
+			host: dev ? "localhost" : "kudadam.com",
+			user: MYSQL_USERNAME,
+			password: MYSQL_PASSWORD,
+			database: MYSQL_DATABASE
+		});
+		await DB.getConnection();
+		return DB;
+	} catch (error) {
+		return error;
+	}
 };
